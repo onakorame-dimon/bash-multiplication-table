752f8fd67bfc2234119e40e579852764e3120133
Odyssey <106778824+CyberOdeyssey@users.noreply.github.com>
2026-01-30 17:30:56 +0100

    feat(script): Basic project requirements complete
    All objectives for program specification implemented
    
    Future work to be completed:
    Enhanced interaction: Repeating without restarting
    Creative display options: Ascending and Descending order of table

--- script.sh
+++ script.sh
@@ -4,7 +4,10 @@
 #===========================================================
 ############################################################
 
-#Full table 
+
+#===========================================================
+#FULL TABLE 
+#===========================================================
 full_table() {
     echo
     
@@ -14,56 +17,123 @@ full_table() {
                 echo "${NUM} x $i = ${product}" 
             done
 }
+#===========================================================
+#===========================================================
 
 
-#Partial Table
-partial_table() {
+#===========================================================
+#PARTIAL TABLE
+#===========================================================
 
-echo 
-read -p "Enter the starting number (between 1 and 10): " a
-echo 
+eval_partial_table() {
 
-read -p "Enter the ending number (between 1 and 10): " b
-echo 
+        if ((b > a ))
+            then
+                echo "The partial multiplicatoin table for ${NUM} for ${a} to ${b}:" 
+                for ((i=a; i<=b; i++))
+                    do 
+                    product=$(( NUM * i)) 
+                    echo "${NUM} x ${i} = ${product}"
+                    done 
+        else 
+            echo -e "Invalid range. Showing full table instead.\n"
 
-if ((b > a ))
-    then
-        echo "The partial multiplicatoin table for ${NUM} for ${a} to ${b}:" 
-        for ((i=a; i<=b; i++))
-          do 
-          product=$(( NUM * i)) 
-          echo "${NUM} x ${i} = ${product}"
-          done 
-else 
-      echo -e "Invalid range. Showing full table instead.\n"
+            echo -e "The full multiplication table for 3:\n"
+            full_table
+        fi
 
-      echo "The full multiplication table for 3: "
-      full_table
-fi 
 }
 
 
-read -p "Do you want a full table or a partial table? (Enter 'f' for full 'p' for partial): " TABLE_LEN
+partial_table() {
+
 echo 
 
+read -p "Enter the starting Integer (between 1 and 10): " a
+echo 
+
+if ! [[ $a =~ ^[1-9][0-9]*$ ]] 
+    then 
+    echo "$a is not a valid integer, Enter a valid integer: "
+    
+elif (( a < 1 || a > 10 ))
+    then 
+    echo "$a is not an integer from 1 to 10. Enter an integer from 1 to 10"
+
+else   
+    read -p "Enter the ending Integer (between 1 and 10): " b
+    echo 
+
+    if ! [[ $b =~ ^[1-9][0-9]*$ ]]
+        then                
+        echo "$b is not a valid integer, Enter a valid integer: "
+    elif (( b < 1 || b > 10 ))
+        then
+        echo "$b is not an integer from 1 to 10. Enter an integer from 1 to 10."
+    
+    else 
+    eval_partial_table    
+    fi
+fi 
+
+}
+
+#===========================================================
+#CHOOSE TABLE TYPE
+#===========================================================
+
+prompt_table_type() {
+
+read -p "Do you want a full table or a partial table? (Enter 'f' for full 'p' for partial): " TABLE_LEN
+
 if [[ $TABLE_LEN == "f" ]] #if TABLE_LEN is full, then full tab
-  then
-  #Full Multiplication Table 
-    read -p "Enter a number for the multiplication table: " NUM
-    full_table
+  then 
+        full_table
 
 elif [[ $TABLE_LEN == "p" ]] #If TABLE_LEN is part, then part tab
   then 
-    read -p "Enter a number for the multiplication table: " NUM
     partial_table
 else
     echo "Enter either p or f "
 
-fi  
+fi 
+
+}
 
 
+#===========================================================
+# VALIDATE INPUT INTEGER
+#===========================================================
 
-#==================================
-#Refactoring into functions
-#==================================
+input_validation() {
 
+echo
+read -p "Enter an integer for the multiplication table: " NUM
+echo 
+
+if [[ $NUM =~ ^[+-]?[1-9][0-9]*$ ]]
+    then 
+        prompt_table_type
+else 
+    echo "${NUM} is not a valid integer. Enter a valid integer: " 
+fi
+
+}
+
+
+#===========================================================
+# INIT PROGRAM
+#===========================================================
+
+init_program() {
+
+input_validation
+
+}
+
+
+#===========================================================
+# RUN THE PROGRAM
+#===========================================================
+
+init_program
