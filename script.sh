604f7b0b30dc8ff1443a3a99dba18d1205d2a7db
Odyssey <106778824+CyberOdeyssey@users.noreply.github.com>
2026-01-30 18:23:45 +0100

    docs: add inline comments for code clarity
    Added comments for project assessment

--- script.sh
+++ script.sh
@@ -4,14 +4,18 @@
 #===========================================================
 ############################################################
 
+# BASH SCRIPT FOR GENERATING MULTIPLICATION TABLE
 
 #===========================================================
 #FULL TABLE 
 #===========================================================
+
+# This function is used to generate a full table 
 full_table() {
     
     echo "The full multiplication table for 3: "
 
+# Using a for loop to generate a multiplication table for NUM for 10 iterations
         for ((i=1; i<=10; i++)) 
             do 
                 product=$(( NUM * i ))
@@ -26,8 +30,10 @@ full_table() {
 #PARTIAL TABLE
 #===========================================================
 
+#This function is used to generate the multiplication table for a partial table
 eval_partial_table() {
 
+# Validate if the range of inputs and out-of-bound enteries
         if ((b > a ))
             then
                 echo "The partial multiplicatoin table for ${NUM} for ${a} to ${b}:" 
@@ -38,16 +44,17 @@ eval_partial_table() {
                     done 
         else 
             echo -e "Invalid range. Showing full table instead.\n"
-
+# Generate a full table if invalid range is provided
             full_table
         fi
 
 }
 
 
-
+# This function validates if the partial table input is a valid integer
 validate_ptable_input() {
 
+#Using Regex to validate if the table is an integer
 if ! [[ $1 =~ ^[1-9][0-9]*$ ]] 
     then 
     echo "$1 is not a valid integer, Enter a valid integer: "
@@ -59,8 +66,10 @@ fi
 
 }
 
+#Calling this function implements all the logic required to generate a table and outputs the table
 partial_table() {
 
+# Prompt user for input
 read -p "Enter the starting Integer (between 1 and 10): " a
 echo 
 validate_ptable_input $a
@@ -77,10 +86,13 @@ eval_partial_table
 #CHOOSE TABLE TYPE
 #===========================================================
 
+# This function is used to prompt the user for the type of table format
 prompt_table_type() {
 
 read -p "Do you want a full table or a partial table? (Enter 'f' for full 'p' for partial): " TABLE_LEN
 echo
+
+# If-elif chain used to decide which table to output based on user's input
 
 if [[ $TABLE_LEN == "f" ]] #if TABLE_LEN is full, then full tab
   then 
@@ -100,6 +112,7 @@ fi
 #===========================================================
 # VALIDATE INPUT INTEGER
 #===========================================================
+#Function to validate if the a valid integer is provided before table is generated
 
 input_validation() {
 
@@ -120,6 +133,7 @@ fi
 # INIT PROGRAM
 #===========================================================
 
+# This function calls the first code to start program execution
 init_program() {
 
 input_validation
@@ -131,4 +145,5 @@ input_validation
 # RUN THE PROGRAM
 #===========================================================
 
+# This function runs the program
 init_program
