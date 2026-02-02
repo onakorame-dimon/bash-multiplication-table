365bc695e5400e79b7cee6330badf19516d86069
Odyssey <106778824+CyberOdeyssey@users.noreply.github.com>
2026-02-02 20:12:54 +0100

    feat(script): Successfully implemented looping mode for repeated execution
    All requirements for project fully implemented.
    
    Future work:
    More colorful banner display
    Option for ascending and descending order of table output
    Limitation on range of input values

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@
 # This function is used to generate a full table 
 full_table() {
     
-    echo "The full multiplication table for 3: "
+    echo "The full multiplication table for ${NUM}: "
 
 # Using a for loop to generate a multiplication table for NUM for 10 iterations
         for ((i=1; i<=10; i++)) 
@@ -31,19 +31,23 @@ full_table() {
 #===========================================================
 
 #This function is used to generate the multiplication table for a partial table
+
 eval_partial_table() {
+
 
 # Validate if the range of inputs and out-of-bound enteries
         if ((b > a ))
             then
-                echo "The partial multiplicatoin table for ${NUM} for ${a} to ${b}:" 
+                echo "The partial multiplicatoin table for ${NUM} for range ( ${a} to ${b} ):" 
                 for ((i=a; i<=b; i++))
                     do 
                     product=$(( NUM * i)) 
                     echo "${NUM} x ${i} = ${product}"
                     done 
         else 
-            echo -e "Invalid range. Showing full table instead.\n"
+            echo -e "Invalid range (starting integer: $a ending integer: $b)"
+            echo -e "Showing full table instead.\n"
+
 # Generate a full table if invalid range is provided
             full_table
         fi
@@ -51,34 +55,134 @@ eval_partial_table() {
 }
 
 
-# This function validates if the partial table input is a valid integer
-validate_ptable_input() {
 
-#Using Regex to validate if the table is an integer
-if ! [[ $1 =~ ^[1-9][0-9]*$ ]] 
-    then 
-    echo "$1 is not a valid integer, Enter a valid integer: "
-    
-elif (( $1 < 1 || $1 > 10 ))
-    then 
-    echo "$1 is not an integer from 1 to 10. Enter an integer from 1 to 10"
-fi
+# Function required to aid generation of partial table
 
-}
-
-#Calling this function implements all the logic required to generate a table and outputs the table
 partial_table() {
 
-# Prompt user for input
-read -p "Enter the starting Integer (between 1 and 10): " a
-echo 
-validate_ptable_input $a
+#Validate input for starting integer 
 
-read -p "Enter the ending Integer (between 1 and 10): " b
-echo 
-validate_ptable_input $b
-    
+p_table=true
+
+while $p_table
+do
+    read -p "Enter a starting Integer (between 1 and 10): " a
+    echo 
+
+        if [[ $a == "q" ]]
+            then 
+            exit
+        fi 
+
+        if ! [[ $a =~ ^[1-9][0-9]*$ ]] 
+            then 
+
+            while $p_table 
+                do
+                read -p "$a is not a valid integer, Enter a valid integer: " a
+                    if  [[ $a =~ ^[1-9][0-9]*$ ]]
+                        then 
+                            if  (( $a < 1 || $a > 10 ))
+                                then 
+                                continue 2 # This specific code logic makes it tricky to refactor the input validation of starting and ending integer
+                            else 
+                                p_table=false
+                                
+                            fi           
+                    else
+                        continue
+                    fi
+            done             
+
+        elif (( $a < 1 || $a > 10 ))
+           then  
+            while $p_table
+            do
+                read -p "$a is not an integer from 1 to 10. Enter an integer from 1 to 10: " a
+                if  [[ $a =~ ^[1-9][0-9]*$ ]]
+                        then 
+                            if  (( $a < 1 || $a > 10 ))
+                                then 
+                                continue 
+                            else 
+                                p_table=false    
+                            
+                            fi           
+                else
+                        continue 2
+                fi    
+   
+            done        
+        else 
+            break
+
+        fi
+
+done
+
+#Validate input for ending integer
+
+p_table=true
+
+while $p_table
+do
+    read -p "Enter an ending Integer (between 1 and 10): " b
+    echo 
+
+         
+        if [[ $b == "q" ]]
+            then 
+            exit
+        fi 
+
+        if ! [[ $b =~ ^[1-9][0-9]*$ ]] 
+            then 
+
+            while $p_table 
+                do
+                read -p "$b is not a valid integer, Enter a valid integer: " b
+                    if  [[ $b =~ ^[1-9][0-9]*$ ]]
+                        then 
+                            if  (( $b < 1 || $b > 10 ))
+                                then 
+                                continue 2
+                            else 
+                                p_table=false
+                                
+                            fi           
+                    else
+                        continue
+                    fi
+            done                
+
+        elif (( $b < 1 || $b > 10 ))
+           then  
+            while $p_table
+            do
+                read -p "$b is not an integer from 1 to 10. Enter an integer from 1 to 10: " b
+                if  [[ $b =~ ^[1-9][0-9]*$ ]]
+                        then 
+                            if  (( $b < 1 || $b > 10 ))
+                                then 
+                                continue 
+                            else 
+                                p_table=false    
+                            
+                            fi           
+                else
+                        continue 2
+                fi    
+   
+            done        
+        else 
+            break
+
+        fi
+
+done
+
 eval_partial_table     
+
 }
 
 
@@ -87,34 +191,79 @@ eval_partial_table
 #===========================================================
 
 # This function is used to prompt the user for the type of table format
+
 prompt_table_type() {
 
-read -p "Do you want a full table or a partial table? (Enter 'f' for full 'p' for partial): " TABLE_LEN
-echo
+prompt_type=true
+while $prompt_type
+    do
+    read -p "Do you want a full table or a partial table? (Enter 'f' for full 'p' for partial): " TABLE_LEN
+    echo
 
-# If-elif chain used to decide which table to output based on user's input
+    if [[ $TABLE_LEN == "q" ]]
+    then 
+        exit 
 
-if [[ $TABLE_LEN == "f" ]] #if TABLE_LEN is full, then full tab
-  then 
-        full_table
+    elif [[ $TABLE_LEN == "f" ]] #if TABLE_LEN is full, then full table
+    then 
+            full_table 
+            prompt_type=false
 
-elif [[ $TABLE_LEN == "p" ]] #If TABLE_LEN is part, then part tab
-  then 
-    partial_table
-else
-    echo "Enter either p or f "
+    elif [[ $TABLE_LEN == "p" ]] #If TABLE_LEN is part, then part table
+    then 
+        partial_table
+        prompt_type=false
+        
+    else
+        while $prompt_type      # Refactoring looks attractive here. But it's significantly 
+                                # different from the previous code block and is part of the if-else logic  
+                                # of the previous block.
+        do
+        read -p "Enter either p or f, or q to quit: "   TABLE_LEN
 
-fi 
+            if [[ $TABLE_LEN == "q" ]]
+            then 
+                exit 
+
+            elif [[ $TABLE_LEN == "f" ]] #if TABLE_LEN is f, then full table
+            then 
+                    full_table
+                    prompt_type=false
+                    
+            elif [[ $TABLE_LEN == "p" ]] #If TABLE_LEN is part, then part tab
+            then 
+                partial_table
+                prompt_type=false
+                
+                
+            else
+                continue
+            fi
+        done
+
+    fi 
+done
 
 }
 
 
 #===========================================================
-# VALIDATE INPUT INTEGER
+# PROMPT INPUT INTEGER
 #===========================================================
-#Function to validate if the a valid integer is provided before table is generated
+#Function to prompt user for input
 
-input_validation() {
+input_prompt() {
+
+input_prompt=true
+
+while $input_prompt
+
+do 
+
+echo -e "\n#====================================================================#"
+echo -e "# Welcome to the Multiplication Table Generator.                     #" 
+echo -e "# To begin, input a number. Enter the letter 'q' to quit the program.#"
+echo -e "#====================================================================#\n"
 
 read -p "Enter an integer for the multiplication table: " NUM
 echo 
@@ -122,12 +271,36 @@ echo
 if [[ $NUM =~ ^[+-]?[1-9][0-9]*$ ]]
     then 
         prompt_table_type
+elif
+    [[ $NUM == "q" ]]
+    then 
+    exit  
+
 else 
-    echo "${NUM} is not a valid integer. Enter a valid integer: " 
+    while $input_prompt
+    do 
+    read -p "${NUM} is not a valid integer. Enter a valid integer (or q to quit): " NUM
+    echo 
+    
+    if [[ $NUM =~ ^[+-]?[1-9][0-9]*$ ]]
+    then 
+        prompt_table_type
+        break
+
+    elif [[ $NUM == "q" ]] 
+    then
+    exit 
+    
+    else  
+        continue
+    fi
+    done
+
 fi
 
-}
+done
 
+}
 
 #===========================================================
 # INIT PROGRAM
@@ -136,7 +309,7 @@ fi
 # This function calls the first code to start program execution
 init_program() {
 
-input_validation
+input_prompt
 
 }
 
